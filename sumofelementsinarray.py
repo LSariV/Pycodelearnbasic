45d06fb58426771569b72c56375f1fe1c260d0e3
lakshmi-apfm <valasapalli@hotmail.com>
2021-12-27 12:08:24 -0800

    finding sum and add and subtracting from sum of array

---
+++ sumofelementsinarray.py
@@ -0,0 +1,18 @@
+#input: arr[] = {1, 2, 3}
+#output: 6
+# 1+ 2+ 3 =6
+  
+  # example : if we have array we can find sum by function 
+
+arr = [1,2,3,4,5]
+
+print("sum of given array is ", sum(arr))  # it gives 15
+
+# adding some value to the sum of array
+
+print("sum of given array + adding 10 to it is " , sum(arr,10)) # 15+10
+
+# subtracting some value from the sum of array
+
+print("subtracting from given sum of array is " , sum(arr,-10)) # 15-10
+
